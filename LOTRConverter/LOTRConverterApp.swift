8aa253e36ddfa5bececfe228109c51634f79b34e
Bhavin B <bhavinbhadani@yahoo.in>
2024-12-04 14:20:32 +0530

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Bhavin Bhadani on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
